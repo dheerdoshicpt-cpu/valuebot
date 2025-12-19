6263c12887e0a645e030efcb7d4152bcd8e48004
ethachu21 <ethachu21@gmail.com>
2025-12-19 04:04:59 +0000

    Update Bot.py
    Update bot.py to use our new yml database system

--- bot.py
+++ bot.py
@@ -3,23 +3,20 @@ from discord import app_commands
 import json
 import os
 import dotenv
+from data import Database, Item
 
 # DO NOT REMOVE
 dotenv.load_dotenv()
 
-def load_items():
-    with open("items.json", "r") as f:
-        return json.load(f)
 
 async def item_autocomplete(interaction: discord.Interaction,current: str):
-    items = load_items().keys()
     current = current.lower()
     choices = []
 
-    for item in items:
-        if current.lower() in item.lower():
+    for item in Database().items:
+        if current.lower() in item.name:
             choices.append(
-                app_commands.Choice(name=item, value=item)
+                app_commands.Choice(name=item.name, value=item.name)
             )
     
     return choices[:25]
@@ -40,15 +37,15 @@ client = MyClient()
 @app_commands.describe(item="Item name (example: cursed volc)")
 @app_commands.autocomplete(item=item_autocomplete)
 async def value_command(interaction: discord.Interaction, item: str):
-    items = load_items()
+    db = Database()
     
-    if item in items:
-        data = items[item]
+    item_obj = db.get_item(item)
+    if item_obj:
         await interaction.response.send_message(
-            f"**{item.title()}**\n"
-            f"💰 Value: **{data['value']}**\n"
-            f"📈 Demand: **{data['demand']}**\n"
-            f"🔥 Overpay: **{data['overpay']}**"
+            f"**{item_obj.name}**\n"
+            f"💰 Value: **{item_obj.value}**\n"
+            f"📈 Demand: **{item_obj.demand}**\n"
+            f"🔥 Overpay: **{item_obj.overpay}**"
         )
     else:
         await interaction.response.send_message(
